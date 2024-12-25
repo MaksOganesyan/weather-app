fbf5cf98eb3288317652eceb48aa57a426e60cd7
MaksOganesyan <maxogan099@gmail.com>
2024-12-25 20:29:47 +0300

    work

---
+++ weather-app-my-backend-branch-sra/project_op/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app import routes
+import uvicorn
+import json
+from flask_cors import CORS
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(routes.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
